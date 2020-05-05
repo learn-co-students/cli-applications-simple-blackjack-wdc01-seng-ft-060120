28837162a0b80deaea3083df4044757b9c9062bb
Kosi Akporji <kosiazom@gmail.com>
2020-05-05 21:38:05 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,72 @@
 def welcome
-  # code #welcome here
+ 
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(total)
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(number)
+  puts "Sorry, you hit #{number}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
-end
+  
+sum  = deal_card + deal_card
 
-def hit?
-  # code hit? here
+   display_card_total(sum)
+  return sum
+  
 end
 
 def invalid_command
-  # code invalid_command here
+  
+  puts "Please enter a valid command"
 end
 
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
+def hit?(current_total)
+
+  prompt_user
+  user_input = get_user_input
+  
+  if user_input == 's'
+    current_total
+  elsif user_input == 'h'
+    new_card = deal_card + current_total
+    new_card
+  else 
+     prompt_user
+    user_input = get_user_input
+  
+    invalid_command
+  end
+end
+
+
 
 def runner
-  # code runner here
+  welcome
+  total = initial_round
+  
+  until total > 21
+ total = hit?(total)
+ display_card_total(total)
 end
+end_game(total)
+end
+
+
     
